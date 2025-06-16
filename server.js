86592b222d2640a245183d7ad82150012f70593b
salehabegum99 <naseemjuveriya88@gmail.com>
2025-06-16 19:54:02 +0530

    khtm

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+const uri = process.env.MONGODB_URI;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -19,9 +20,18 @@ app.use("/api/events", eventRoutes);
 app.use("/api/partner", partnerRoutes);
 app.use("/api/awareness", awarenessRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
+if (!uri) {
+  console.error("Missing MONGODB_URI in environment");
+  process.exit(1); // Stop if undefined
+}
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.error("MongoDB connection error:", err));
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
